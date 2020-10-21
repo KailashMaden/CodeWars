af11f30b7584201cfb8ff3f584dbabd80029f6c6
kailashmaden <nedamkailash12@gmail.com>
2020-10-21 19:30:52 +0545

    problem 2

--- codewarsProblem.js
+++ codewarsProblem.js
@@ -7,3 +7,17 @@ const songDecoder = (song) => song.replace(/(WUB)+/g, ' ').trim();
 
 const result = songDecoder('WUBWEWUBWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIEND');
 console.log(result);
+
+
+
+// Problem 2
+// Input: When an array is passed like [19, 5, 42, 2, 77]
+// Output: should be 5+2 = 7
+
+const sumTwoSmallestNumbers = (numbers) => {
+  const [firstNumber, secondNumber] = numbers.sort((a, b) => a - b);
+  return firstNumber + secondNumber;
+};
+
+const numbers = [19, 5, 42, 2, 77];
+console.log(sumTwoSmallestNumber(numbers));
