28188cd3c5071e84d6e81fea8f1472a54d47a540
kailashmaden <nedamkailash12@gmail.com>
2020-10-27 20:40:02 +0545

    Problem 8

--- codewarsProblem.js
+++ codewarsProblem.js
@@ -121,11 +121,30 @@ function createPhoneNumber(numbers) {
 // Option solution
 // function createPhoneNumber(numbers){
 //   var format = "(xxx) xxx-xxxx";
-  
+
 //   for(var i = 0; i < numbers.length; i++)
 //   {
 //     format = format.replace('x', numbers[i]);
 //   }
-  
+
 //   return format;
+// }
+
+//------------------------------------- Problem 8
+
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+// each taken only once - coming from s1 or s2.
+// Example: a = "xyaabbbccccdefww"
+// b = "xxxxyyyyabklmopq"
+// longest(a, b) -> "abcdefklmopqwxy"
+
+const longest = (s1, s2) => [...new Set(s1 + s2)].sort().join('');
+
+// Optional solution
+// function longest(s1, s2) {
+//   var answer = [s1, s2].join("").split("").sort();
+//   return Array.from( new Set(answer)).join("");
+// }
+// function longest(s1, s2) {
+//   return (s1+s2).split('').sort().join('').replace(/(.)\1+/g, '$1');
 // }
